c6f8c351b27eeaf732a71f4ed44b23e5af601645
Tanner Collin <git@tannercollin.com>
2019-08-07 21:01:47 +0000

    Move t0url to its own git repo

---
+++ t0url.py
@@ -0,0 +1,96 @@
+# t0url - url.t0.vc
+# MIT License
+
+import random
+import shelve
+import string
+import validators
+
+from flask import abort, Flask, request, redirect
+
+DB = 'data/t0url'
+PORT = 5004
+DOMAIN = 'url.t0.vc'
+URL = 'https://' + DOMAIN
+POST = 'url'
+
+def help():
+    form = (
+        '<form action="{0}" method="POST" accept-charset="UTF-8">'
+	'<input name="web" type="hidden" value="true">'
+        '<input name="{1}">'
+        '<br><button type="submit">Submit</button></form>'.format(URL, POST)
+    )
+    return """
+<pre>
+                        url.t0.vc
+NAME
+    t0url: command line short URL.
+
+USAGE
+    &lt;command&gt; | curl -F '{0}=&lt;-' {1}
+    or upload from the web:
+
+{2}
+
+EXAMPLES
+    ~$ cat yourfile | curl -F '{0}=&lt;-' {1}
+       {1}/VFAR
+    ~$ firefox {1}/VFAR
+
+    Add this to your .bashrc:
+
+    alias {0}="curl -F '{0}=<-' {1}"
+
+    Now you can pipe a URL directly into {0}!
+
+SOURCE CODE
+    https://txt.t0.vc/RWFA
+
+SEE ALSO
+    https://txt.t0.vc
+    https://pic.t0.vc
+</pre>""".format(POST, URL, form)
+
+def new_id():
+    return ''.join(random.choice(string.ascii_uppercase) for _ in range(4))
+
+flask_app = Flask(__name__)
+
+@flask_app.route('/', methods=['GET'])
+def index():
+    return '<html><body>{}</body></html>'.format(help())
+
+@flask_app.route('/', methods=['POST'])
+def new():
+    try:
+        with shelve.open(DB) as db:
+            nid = new_id()
+            while nid in db:
+                nid = new_id()
+
+            url = request.form['url'].strip()
+
+            if not url: raise
+            if DOMAIN in url: raise
+            if not url.startswith('http'):
+                url = 'http://' + url
+            if not validators.url(url): raise
+
+            db[nid] = url
+
+            print('Adding url {}:\n{}'.format(nid, url))
+
+        return '{}/{}'.format(URL, nid) + '\n'
+    except:
+        abort(400)
+
+@flask_app.route('/<nid>', methods=['GET'])
+def get(nid):
+    try:
+        with shelve.open(DB) as db:
+            return redirect(db[nid])
+    except:
+        abort(404)
+
+flask_app.run(port=PORT)
